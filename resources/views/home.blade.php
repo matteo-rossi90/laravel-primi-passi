e28d445b19ddd12ced6c56e3dd8c3b456c2a758d
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-09-09 15:34:18 +0200

    feat: print an object collection list by foreach loop and set conditions

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel - Primi passi</title>
+    <style>
+
+        ul{
+            list-style-type: none;
+        }
+        .red {
+            color: red;
+        }
+
+        .green {
+            color: green;
+        }
+
+    </style>
 </head>
 <body>
 
-    <h1>Hello world!</h1>
+    {{-- stampare il titolo --}}
+    <h1>{{ $title }}</h1>
+
+    {{--  stampare il sottotitolo --}}
+    <h2>{{ $merch }}</h2>
+
+    {{-- stampare gli elementi di cartoleria attraverso il foreach --}}
+    <ul>
+        @foreach($objects as $articolo)
+            {{-- se l'articolo è venduto sarà stampato in rosso --}}
+            @if($articolo['sold'])
+                <li class="red">{{$loop->iteration}} - {{ $articolo['name']}} - {{ $articolo['category']}}</li>
+            {{-- altrimenti in verde --}}
+            @else
+                <li class="green">{{$loop->iteration}} - {{ $articolo['name']}} - {{ $articolo['category']}}</li>
+            @endif
+        @endforeach
+
+    </ul>
 
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,46 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+    //titolo della pagina principale
+    $title = 'Cartoleria';
+
+    //sottotitolo
+    $merch = 'Oggetti in vendita';
+
+    //aggiungere un elenco di oggetti da cancelleria
+
+    $objects = [
+        [
+            'name'=> 'fogli A4',
+            'category' => 'carta',
+            'sold' => true
+        ],
+        [
+            'name' => 'pastelli',
+            'category' => 'colori',
+            'sold' => true
+        ],
+        [
+            'name' => 'penne a sfera',
+            'category' => 'penne per scrivere',
+            'sold' => false
+        ],
+        [
+            'name' => 'carta trasparente',
+            'category' => 'carta',
+            'sold' => false
+        ],
+        [
+            'name' => 'temperamatite',
+            'category' => 'oggetti e strumenti',
+            'sold' => true
+        ],
+        [
+            'name' => 'evidenziatori',
+            'category' => 'oggetti e strumenti',
+            'sold' => false
+        ]
+    ];
+    return view('home', compact( 'title', 'merch', 'objects'));
 });
